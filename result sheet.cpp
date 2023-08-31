796898fa1b743f2bb4f82f67e42a2365e7c84c6a
samiul-5 <136092047+samiul-5@users.noreply.github.com>
2023-08-31 14:52:33 +0600

    Add files via upload

---
+++ result sheet.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Course {
+protected:
+    string courseName;
+    string teacherName;
+    double obtainMarks;
+
+public:
+    Course(const string& name, const string& teacher, double marks)
+        : courseName(name), teacherName(teacher), obtainMarks(marks) {}
+
+    virtual void display() = 0;
+    virtual double calculatePoints() = 0;
+};
+
+class ObjectOrientedProgram : public Course {
+public:
+    ObjectOrientedProgram()
+        : Course("Object Oriented Program", "Mahadi Hasan", 4.00) {}
+
+    void display() {
+        cout << "Course name: " << courseName << "\n";
+        cout << "Teacher name: " << teacherName << "\n";
+        cout << "Obtain marks: " << obtainMarks << "\n";
+    }
+
+    double calculatePoints()  {
+        return obtainMarks;
+    }
+};
+
+class DataStructure : public Course {
+public:
+    DataStructure()
+        : Course("Data Structure", "Sezan Mahmud", 3.75) {}
+
+    void display() {
+        cout << "Course name: " << courseName << "\n";
+        cout << "Teacher name: " << teacherName << "\n";
+        cout << "Obtain marks: " << obtainMarks << "\n";
+    }
+
+    double calculatePoints()  {
+        return obtainMarks;
+    }
+};
+
+class DiscreteMath : public Course {
+public:
+    DiscreteMath()
+         :Course("Discrete Math", "Rumana Khatun", 4.00) {}
+
+    void display() override {
+        cout << "Course name: " << courseName << "\n";
+        cout << "Teacher name: " << teacherName << "\n";
+        cout << "Obtain marks: " << obtainMarks << "\n";
+    }
+
+    double calculatePoints()  {
+        return obtainMarks;
+    }
+};
+
+class Math : public Course {
+public:
+    Math()
+        : Course("Math", "Fatema Khatun", 3.00) {}
+
+    void display() override {
+        cout << "Course name: " << courseName << "\n";
+        cout << "Teacher name: " << teacherName << "\n";
+        cout << "Obtain marks: " << obtainMarks << "\n";
+    }
+
+    double calculatePoints() {
+        return obtainMarks;
+    }
+};
+
+class Student {
+private:
+    string studentName;
+    int id;
+    double cgpa;
+   vector<Course*> courses;
+
+public:
+    Student(const string& name, int studentId, double studentCgpa)
+        : studentName(name), id(studentId), cgpa(studentCgpa) {}
+
+    void addCourse(Course* course) {
+        courses.push_back(course);
+    }
+
+    void displayResultSheet() {
+        cout << "I am a student. In Northern University\n";
+        cout << "Student Name: " << studentName << "\n";
+        cout << "Id: " << id << "\n";
+        cout << "Previous cgpa: " << cgpa << "\n";
+        cout << "Program: CSE\n";
+        cout << "_______________________\n";
+
+        double totalPoints = 0.0;
+        for (Course* course : courses) {
+            course->display();
+            totalPoints += course->calculatePoints();
+        }
+
+        double average = totalPoints / courses.size();
+        cout << "Total Point: " << average << "\n";
+
+        if (average >= 3) {
+            cout << "Good Student\n";
+        } else {
+            cout << "Bad Student\n";
+        }
+    }
+
+    ~Student() {
+        for (Course* course : courses) {
+            delete course;
+        }
+    }
+};
+class result{
+    public:
+virtual void resultQuality()=0;
+
+};
+class Final : public result{
+public:
+    void resultQuality(){
+    cout<<"pass"<<endl;
+    }
+
+};
+int main() {
+    Student student("SAMIUL HOSSAIN",415, 3.81);
+    student.addCourse(new ObjectOrientedProgram());
+    student.addCourse(new DataStructure());
+    student.addCourse(new DiscreteMath());
+    student.addCourse(new Math());
+
+    student.displayResultSheet();
+    result *r;
+    Final f;
+    r=&f;
+    r->resultQuality();
+    return 0;
+}
